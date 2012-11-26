9dec413876dc80648881436f83e39b7035ad3222
achooley <alexander.chuley@gamepoint.com.ua>
2012-11-26 12:20:44 +0200

    add new text

--- StarladerNews/src/com/AlisaSoft/starladernews/MainActivity.java
+++ StarladerNews/src/com/AlisaSoft/starladernews/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
         //Code changes
+        //add new code
     }
 
     
