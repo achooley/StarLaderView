1a57e18f505084f7811335f04d4aaa20b7321b7c
achooley <alexander.chuley@gamepoint.com.ua>
2012-11-27 14:54:40 +0200

    add code for login vkontakte server

--- StarladerNews/src/com/AlisaSoft/starladernews/MainActivity.java
+++ StarladerNews/src/com/AlisaSoft/starladernews/MainActivity.java
@@ -1,17 +1,44 @@
 package com.AlisaSoft.starladernews;
 
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
 
+   WebView web; 	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+        
+        web=(WebView)findViewById(R.id.WebLogin);
+        CookieManager manager=CookieManager.getInstance();
+        manager.removeAllCookie();
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("https://oauth.vk.com/authorize?client_id=2827360&scope=audio&redirect_uri=http://oauth.vk.com/blank.html&display=wap&response_type=token");
+        web.setWebViewClient(new WebViewClient(){
+        	@Override
+        	public void onPageFinished(WebView view, String url){
+        		Log.e("URL ",url);
+        		super.onPageFinished(view, url);
+        	}        	
+        });
+         
+       
+        
+        
+        		      
+      
     }
 
     @Override
@@ -22,5 +49,5 @@ public class MainActivity extends Activity {
         //add new code
     }
 
-    
+ 
 }
